56f404697806285e2d7e0d072c5c046fe68c76a5
Riski Afendi <riskistar2703@gmail.com>
2024-09-16 20:03:09 +0700

    add operator.kt

--- src/main/kotlin/operator.kt
+++ src/main/kotlin/operator.kt
@@ -1,5 +1,7 @@
+/*
 fun main(args: Array<String>) {
-  /*  var maybeNumber : Int? = 15
+
+    var maybeNumber : Int? = 15
     maybeNumber = null
     println(maybeNumber * 2 )
 
@@ -8,5 +10,30 @@ fun main(args: Array<String>) {
     println(s1.length)
     println(s2.length)
 
-   */
+
+
+
+}
+
+ */
+
+/*
+fun main(args: Array<String>) {
+    //Null safety
+    //The !! operator
+    var maybeNumber: Int? = 15
+    println(maybeNumber!! * 2)
+    maybeNumber = null
+    println(maybeNumber!! * 2)
+}
+
+ */
+
+fun main(args: Array<String>) {
+    //Conditions
+    var maybeNumber: Int? = 15
+    if (maybeNumber != null)
+        println(maybeNumber * 2)
+    else
+        println("The entered value isn't a number")
 }
